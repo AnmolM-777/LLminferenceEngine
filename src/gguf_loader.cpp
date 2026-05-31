9b3bce3c541ca50a743581cfc99778c761c01f05
Anmol Mishra <anmolmishra777@gmail.com>
2026-05-31 13:43:14 +0530

    fix(loader): validate GGUF magic header bytes before allocation [step 49]

--- src/gguf_loader.cpp
+++ src/gguf_loader.cpp
@@ -7,3 +7,5 @@ std::vector<float> GGUFLoader::get_tensor(const std::string& name) { return std:
 // Incremental development step #19
 
 // Incremental development step #34
+
+// Incremental development step #49
