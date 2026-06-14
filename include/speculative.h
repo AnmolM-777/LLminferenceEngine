2680d3a428b9326482bef1720639e6b453286cc7
Anmol Mishra <anmolmishra777@gmail.com>
2026-06-14 12:45:15 +0530

    docs(readme): update llama.cpp benchmark throughput tables [step 71]

--- include/speculative.h
+++ include/speculative.h
@@ -11,3 +11,5 @@ class SpeculativeDecoder { public: static bool verify_draft_tokens(const std::ve
 // Incremental development step #41
 
 // Incremental development step #56
+
+// Incremental development step #71
